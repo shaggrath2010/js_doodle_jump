3a07a10a85dfa2bae7ee5a4578f2d9f3ccf6239b
Mirza Sergey <shaggrath@mail.ru>
2016-01-04 16:16:41 +0200

    first example

--- media/js/engine.js
+++ media/js/engine.js
@@ -1,5 +1,32 @@
 /*global jQuery*/
 "use strict";
 (function($) {
+	var engine = new Engine();
+	$.fn.extend({
+		doodleInit: function() {
+			engine.physics.jump(this);
+		}
+	});
 
+	function Engine() {
+		var state = this;
+		state.physics = {};
+		state.physics.jump = function(obj) {
+			var bottomHeight = parseInt(obj.css('bottom')),
+				jumpHeight = 200,
+				speed = 500;
+			console.log(speed / 1000);
+			var timer = setInterval(function() {
+				if (jumpHeight === bottomHeight) {
+
+					obj.attr('style', 'transition: bottom ' + speed / 1000 + 's ease-in;');
+					bottomHeight = 0;
+				} else {
+					obj.attr('style', 'transition: bottom ' + speed / 1000 + 's ease-out;');
+					bottomHeight = jumpHeight;
+				}
+				obj.css('bottom', bottomHeight);
+			}, speed * 1.04);
+		};
+	}
 })(jQuery);
